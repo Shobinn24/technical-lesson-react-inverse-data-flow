0fdc723b16bd427858ba57c1afcb88e88ed86fc6
shobinn24 <shobinn24@gmail.com>
2026-01-09 14:39:01 -0500

    technical react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import EmployeeForm from './components/EmployeeForm'
 import employeeData from "./data/employees";
@@ -7,12 +7,29 @@ import EmployeeList from './components/EmployeeList';
 
 function App() {
   const [employees, setEmployees] = useState(employeeData)
-
+  function handleDelete(employeeToDelete){
+    const newArray = employees.filter(employee =>{
+      if(employee.id==employeeToDelete.id){
+        return false
+      }
+      return true
+    })
+    setEmployees(newArray)
+  }
+  function handleEdit(employeeToEdit){
+    const newArray = employees.map(employee=>{
+      if(employee.id==employeeToEdit.id){
+        return employeeToEdit
+      }
+      return employee
+    })
+    setEmployees(newArray)
+  }
   return (
     <div>
       <Header/>
       <EmployeeForm employees={employees} setEmployees={setEmployees}/>
-      <EmployeeList employees={employees}/>
+      <EmployeeList employees={employees} handleDelete={handleDelete} handleEdit={handleEdit}/>
     </div>
   )
 }
--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,24 +1,44 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-function Employee({employee}) {
+function Employee({employee,handleEdit,handleDelete}) {
   const [edit,setEdit] = useState(false)
   const [roleEdit,setroleEdit] = useState(employee.role)
 
+  function handleSubmit(e){
+    e.preventDefault()
+    const editedEmployee = {
+      ...employee,
+      role: roleEdit
+    }
+    handleEdit(editedEmployee)
+    setEdit(false)
+  }
   return (
     <tr>
         <td>{employee.firstName}</td>
         <td>{employee.lastName}</td>
         <td onClick={()=>setEdit(true)}>{
               edit ? 
-              <form>
+              <form onSubmit={(e)=>handleSubmit(e)} >
                 <input onChange={(e) => setroleEdit(e.target.value)} value={roleEdit}/>
               </form>
               : employee.role}</td>
         <td>
-          <button> X </button>
+          <button onClick={()=>handleDelete(employee)}> X </button>
         </td>
     </tr>
   );
 }
+
+Employee.propTypes = {
+  employee: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    role: PropTypes.string
+  }).isRequired,
+  handleEdit: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired
+};
 
 export default Employee;
